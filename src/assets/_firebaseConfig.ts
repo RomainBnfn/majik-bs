c1d995a4949d4b1639653f6f1b45e577a66b6d93
RomainBnf <romain.bonnefon@elazur.com>
2026-03-13 10:49:37 +0100

    Add firebase & config

---
+++ src/assets/_firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "xx",
+    authDomain: "xx",
+    databaseURL: "xx",
+    projectId: "xx",
+    storageBucket: "xx",
+    messagingSenderId: "xx",
+    appId: "xx"
+};
